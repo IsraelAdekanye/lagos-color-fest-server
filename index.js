211d8a0b2ac5075419aacfe9534e128d1d9eb978
IsraelMacBook <israeladedigba@yahoo.com>
2022-11-02 11:53:34 +0100

    SECOND commit

--- index.js
+++ index.js
@@ -23,7 +23,14 @@ app.use(cors({
 
 app.use('/register', cors(), attendeeRegRouter);
 app.use('/getAll', cors(), getAttendeesRouter);
-
+app.get('/', async (req, res)=>{
+    try {
+        const {rows} = await pgPool.query(`SELECT * FROM attendees`);
+        res.status(200).json(rows)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
 pgPool.connect().then( ()=> {
     app.listen(process.env.PORT, () => {
         console.log(`You are Connected to PostgreSQL Database and Server has started on port ${process.env.PORT} at ${Date.now()}`)
